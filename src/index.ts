0bdf34ddfdd5fb76bf9aa56c6d8630cc6a2a4741
Francy Santos <francy.horray@gmail.com>
2024-07-24 21:12:43 -0300

    :bug: removed AUTH_URL env secret

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,25 @@ import { Env } from "types/bindings";
 
 // ROUTES
 import users from "@routes/users";
+import { cors } from "hono/cors";
 
 const app = new Hono<{ Bindings: Env }>().basePath("/v1");
 
-// app.use("*", initAuthConfig(getAuthConfig));
-// app.use("/auth/*", authHandler());
-// app.use("/*", verifyAuth());
+app.use("*", initAuthConfig(getAuthConfig));
+app.use("/auth/*", authHandler());
+app.use("/*", verifyAuth());
 
-// app.route("/users", users);
+app.route("/users", users);
 
 app.get("/test", async (c) => {
-  return c.text(`VARIABLE: ${c.env.GOOGLE_CLIENT_SECRET}`);
+  return c.json({
+    AUTH_SECRET: c.env.AUTH_SECRET,
+    AUTH_DRIZZLE_URL: c.env.AUTH_DRIZZLE_URL,
+    DATABASE_URL: c.env.DATABASE_URL,
+    GOOGLE_CLIENT_ID: c.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: c.env.GOOGLE_CLIENT_SECRET,
+    PROJECT_NAME: c.env.PROJECT_NAME,
+  });
 });
 
 export default app;
--- src/types/bindings.ts
+++ src/types/bindings.ts
@@ -3,4 +3,7 @@ export type Env = {
   AUTH_SECRET: string;
   GOOGLE_CLIENT_ID: string;
   GOOGLE_CLIENT_SECRET: string;
+  AUTH_DRIZZLE_URL: string;
+  AUTH_URL: string;
+  PROJECT_NAME: string;
 };
